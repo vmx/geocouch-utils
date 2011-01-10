47f6006a3bdce25bac31a7199f16588a5e604b20
Mick Thompson <mick@collecta.com>
2011-01-09 18:29:27 -0800

    add proximity clustering

--- couchapp/lists/proximity-clustering.js
+++ couchapp/lists/proximity-clustering.js
@@ -2,6 +2,25 @@
  * A clustering algorithm that clusters object based on their proximity, using a threshold value.
  */
 function(head, req) {	
-	start({"code": 501,"headers":{"Content-Type" : "text/plain"}});
-	//TODO: implement proximity clustering list    
-}
+    log("called")
+    var g = require('vendor/clustering/ProximityCluster'),
+        row,
+        threshold =100;
+
+    log("called2")
+ 
+    start({"headers":{"Content-Type" : "application/json"}});
+    if ('callback' in req.query) send(req.query['callback'] + "(");
+   
+    if('threshold' in req.query){ threshold = req.query.threshold;}
+    var pc = new g.PointCluster(parseInt(threshold));   
+ 
+    while (row = getRow()) {
+        pc.addToClosestCluster(row.value);
+    }
+    log("clusters: "+pc.clusters.length);
+    send(JSON.stringify({"rows":pc.getClusters()}));
+
+    if ('callback' in req.query) send(")");
+};
+
--- couchapp/vendor/clustering/ProximityCluster.js
+++ couchapp/vendor/clustering/ProximityCluster.js
@@ -2,11 +2,109 @@
  * Proximity based clustering
  */
 (function() {
-	var cluster = this.cluster = {};
-
+    log("pc called")
+    var proxcluster = this.proxcluster = {};
 	if (typeof module !== 'undefined' && module.exports) {
-		module.exports = cluster;
+		module.exports = proxcluster;
 	}
-	
-	//TODO
+    log("pc called2")
+    var gju = require('vendor/geojson-js-utils/geojson-utils');
+
+    proxcluster.PointCluster = function(threshold){
+        this.clusters = [];
+        this.points = [];
+        this.distanceThreshold = threshold;
+
+    };
+  
+    proxcluster.PointCluster.prototype.addToClosestCluster = function(point) {
+        var distance = 40000; // Some large number
+        var clusterToAddTo = null;
+        var pos = point.geometry;
+        for (var i = 0, cluster; cluster = this.clusters[i]; i++) {
+            var center = cluster.center;
+          
+            if (center) {
+                var d = gju.pointDistance(center, pos)/1000; //convert to km
+                if ( d < this.distanceThreshold) {
+                    distance = d;
+                    clusterToAddTo = cluster;
+                }
+            }
+        }
+
+        if (clusterToAddTo) {
+            clusterToAddTo.addPoint(point);
+        } else {
+            var cluster = new proxcluster.Cluster(this);
+            cluster.addPoint(point);
+            this.clusters.push(cluster);
+        }
+    };
+
+    proxcluster.PointCluster.prototype.getClusters = function(){
+
+        var clusterdata = [];
+        for (var i = 0, cluster; cluster = this.clusters[i]; i++) {
+            clusterdata.push({"center":cluster.center, "points":cluster.getPoints(), "size": cluster.getSize()});
+        }
+        return clusterdata;
+    }
+
+    /**
+     * A cluster that contains points.
+     *
+     * @param {PointCluster} The PointCluster that this
+     *     cluster is associated with.
+     * @constructor
+     * @ignore
+     */
+    proxcluster.Cluster = function (PointCluster) {
+        this.pointCluster = PointCluster;
+        this.center = null;
+        this.points = [];
+    }
+
+    /**
+     * Add a point the cluster.
+     *
+     * @param {point} The point to add.
+     * @return {boolean} True if the point was added.
+     */
+    proxcluster.Cluster.prototype.addPoint = function(point) {
+        if (!this.center) {
+            this.center = point.geometry;
+        } else {
+            var l = this.points.length + 1;
+            var lat = (this.center.coordinates[0] * (l-1) + point.geometry.coordinates[0]) / l;
+            var lng = (this.center.coordinates[1] * (l-1) + point.geometry.coordinates[1]) / l;
+            this.center = {"type": "Point", "coordinates":[lat, lng]};
+        }
+
+      
+        this.points.push(point);
+
+        return true;
+    };
+
+
+    /**
+     * Returns points in the cluster
+     *
+     * @return {Array.<points>} The points
+     */
+    proxcluster.Cluster.prototype.getPoints = function() {
+        return this.points;
+    };
+
+    /**
+     * Returns number of points
+     *
+     * @return {int} number of points
+     */
+    proxcluster.Cluster.prototype.getSize = function() {
+        return this.points.length;
+    };
+
+
 })();
